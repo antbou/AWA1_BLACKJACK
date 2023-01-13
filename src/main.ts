19cdbe3762aa9cb5433fdb1e1df510f02a37fa6b
Anthony <bouillant.anthony@gmail.com>
2023-01-13 14:19:12 +0100

    Create homepage logic

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,37 @@
-import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import store from "./store/index";
+import { createApp, watch, h } from "vue";
 
 import "./assets/main.css";
 
 const app = createApp(App);
-
 app.use(router);
+app.use(store);
+
+app.component("App", {
+  setup() {
+    store.dispatch("initialiseStore");
+
+    watch(
+      () => store.state,
+      (state) => {
+        localStorage.setItem("store", JSON.stringify(state));
+      },
+      { deep: true }
+    );
+
+    // Fetch deck and preload card images
+    store.state.deck.forEach((card: any) => {
+      const img = new Image();
+      img.src = `images/cards/${card.image}`;
+    });
+    // Preload back card image too
+    const img = new Image();
+    img.src = "images/cards/back.png";
+
+    return () => h(App);
+  },
+});
 
 app.mount("#app");
