41f3378b23f20fee51c4a23b2d06f6e503112212
Antho <bouillant.anthony@gmail.com>
2023-01-14 23:52:26 +0100

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  darkMode: "class", // or 'media' or 'class'
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        montserrat: ["Montserrat"],
+        roboto: ["Roboto"],
+      },
       colors: {
-        primary: "#1E1E1E",
-        secondary: "#2E2E2E",
+        primary: "#FF2943", // #FF2943 - #EF233C
+        "primary-light": "#D90429",
+        secondary: "#2172a4",
+        "secondary-highlight": "#30a9f3",
+        disabled: "#707374",
+        "bg-light": "#8D99AE",
+        bg: "#2B2D42",
+        "body-bg": "#202125",
+        "blackjack-light": "#287d31",
+        "blackjack-dark": "#216528",
       },
     },
   },
