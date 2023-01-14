3a4ac2be81d93dfd6fe754081ce4320f299f832b
Antho <bouillant.anthony@gmail.com>
2023-01-14 23:55:16 +0100

    used pinia insead of vuex

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,14 @@
 import App from "./App.vue";
 import router from "./router";
-import store from "./store/index";
 import { createApp, watch, h } from "vue";
-
+import { createPinia } from "pinia";
 import "./assets/main.css";
+import "@fontsource/montserrat";
+import "@fontsource/roboto";
 
+const pinia = createPinia();
 const app = createApp(App);
 app.use(router);
-app.use(store);
-
-app.component("App", {
-  setup() {
-    store.dispatch("initialiseStore");
-
-    watch(
-      () => store.state,
-      (state) => {
-        localStorage.setItem("store", JSON.stringify(state));
-      },
-      { deep: true }
-    );
-
-    // Fetch deck and preload card images
-    store.state.deck.forEach((card: any) => {
-      const img = new Image();
-      img.src = `images/cards/${card.image}`;
-    });
-    // Preload back card image too
-    const img = new Image();
-    img.src = "images/cards/back.png";
-
-    return () => h(App);
-  },
-});
+app.use(pinia);
 
 app.mount("#app");
